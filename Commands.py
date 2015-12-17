49373868224eed2e35f180eb694e642cd8fed114
jwlehman <jeremylehman9@gmail.com>
2015-12-17 18:57:27 -0500

    add inventory function

--- Commands.py
+++ Commands.py
@@ -6,31 +6,37 @@ import Inventory
 def processCommand(msg,player):
     cmd, args = msg.split(" ")
 
-    if(cmd == "help"):
+    if cmd == "help":
         print("\n======Welcome to the Rooms:The RPG Help Section======\n\nCommands you can use are as follows:\nn - head north\nw - head west\ns - head south")
         print("w - head west\nlook - states current room and surroundings\nstatus - states your current status\ninv - takes you to inventory management\nscore - gives current score\nexit - exit the game/n")
-    elif(cmd == "n"):
+    elif cmd == "n":
         player.enterRoom(player.current_room.exits["north"])
-    elif(cmd == "e"):
+    elif cmd == "e":
         player.enterRoom(player.current_room.exits["east"])
-    elif(cmd == "s"):
+    elif cmd == "s":
         player.enterRoom(player.current_room.exits["south"])
-    elif(cmd == "w"):
+    elif cmd == "w":
         player.enterRoom(player.current_room.exits["west"])
-    elif(cmd == "look"):
+    elif cmd == "look":
         print(player.current_room)
-    elif(cmd == "status"):
+    elif cmd == "status":
         print("\n=====%d's Status=====\n\nHealth: %d" % player.health)
-    elif(cmd == "exit"):
+    elif cmd == "exit":
        player.isAlive = False 
        print("\n=====YOU HAVE FAILED TO ESCAPE=====")
-    elif(cmd == "score"):
+    elif cmd == "score":
         print("\n====%s's score====\nEnemies Killed: %d\nDamsels Rescued: %d\nItems Recovered: %d\nTotal Score: %d"%(player.name,player.enemies,player.damsels,player.items,player.get_total_score()))
-    elif(cmd == "inv"):
-        i = player.inventory.head
-        while i is not None:
-            print(i.name)
-            print("\n=====Inventory Manager=====\n\n")
+    elif cmd == "inv":
+        inventor(player)
+    else:
+        print("\nI don't understand what you are asking. Enter 'help' if you do not know what to do.\n")
+
+
+def inventor(player):
+    i = player.inventory.head
+    while i is not None:
+        print(i.name)
+        print("\n=====Inventory Manager=====\n\n")
         while True:
             ans = input("Type 'help' if you would like to see a list of commands for the inventory")
             #exit inventory
@@ -51,7 +57,7 @@ def processCommand(msg,player):
             elif obj == None:
                 print("You don't have that item")
             elif cmd == "examine":
-                    print (obj)
+                print (obj)
             elif cmd == "use":
                 obj.use(player)
             elif cmd == "drop":
@@ -66,11 +72,6 @@ def processCommand(msg,player):
                     i = i.next
                 else:
                     pass
-    else:
-        print("\nI don't understand what you are asking. Enter 'help' if you do not know what to do.\n")
-
-
-
 
 
 
@@ -78,8 +79,7 @@ def processCommand(msg,player):
 
 
     
-     #helo   
-    
+
 
 
  
